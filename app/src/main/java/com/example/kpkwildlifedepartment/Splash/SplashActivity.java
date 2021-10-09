e2d819baa69324fdb212f02d5843dd8f954a6060
Faizan941377 <Faizan941377@gmail.com>
2021-10-09 09:43:46 +0500

    Personal Info form commit

---
+++ app/src/main/java/com/example/kpkwildlifedepartment/Splash/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.kpkwildlifedepartment.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.kpkwildlifedepartment.HumanRecourse.Personal_InfoActivity;
+import com.example.kpkwildlifedepartment.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, Personal_InfoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
